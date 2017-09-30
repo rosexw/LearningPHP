0e0fa4b92db73bfd6f9af291a5f82359bf8784a7
Rose Wan <rose.wan@me.com>
2017-09-29 22:46:12 -0400

    POST request to create a new page - process.php /process

--- index.php
+++ index.php
@@ -5,25 +5,34 @@
   <head>
     <title> Wordpress </title>
   </head>
+
   <body>
+    <form action="process.php" method="post">
+      Enter your name: <input name="name" type = "text">
+      <input type = "submit">
+    </form>
+
     <?php
 
+// define variables, sums, concatenation
     // $myvar = "This is a variable";
     // $number = 5;
     // $number2 = 3;
     // $name = "Rose";
+    // $sum = $number + $number2;
+    // echo $sum;
     // echo "Hello, " . $name . "!";
     // echo "<p><b>hello</b></p>";
     // echo "<p>hello2</p>";
+// if/else statements
+    // $loggedIn = false;
+    // if ($loggedIn == true) {
+    //   echo "You are logged in";
+    // } else {
+    //   echo "Please log in";
+    // }
 
-    $loggedIn = false;
-    if ($loggedIn == true) {
-      echo "You are logged in";
-    } else {
-      echo "Please log in";
-    }
-
-
+// what came with WordPress, but shows an error: "Error establishing a database connection"
     // /**
     //  * Front to the WordPress application. This file doesn't do anything, but loads
     //  * wp-blog-header.php which does and tells WordPress to load the theme.
---
+++ process.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Hello World</title></head>
+
+<body>
+  <?php
+    $name = $_POST["name"];
+    echo "Hello, " . $name;
+  ?>
+
+</body>
+</html>
