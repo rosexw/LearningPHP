08ddf0d3c0e72d817e47fe1d3d6e78ece90e4083
Rose Wan <rose.wan@me.com>
2017-09-29 22:17:28 -0400

    if/else statements in PHP

--- index.php
+++ index.php
@@ -8,13 +8,21 @@
   <body>
     <?php
 
-    $myvar = "This is a variable";
-    $number = 5;
-    $number2 = 3;
-    $name = "Rose";
-    echo "Hello, " . $name . "!";
-    echo "<p><b>hello</b></p>";
-    echo "<p>hello2</p>";
+    // $myvar = "This is a variable";
+    // $number = 5;
+    // $number2 = 3;
+    // $name = "Rose";
+    // echo "Hello, " . $name . "!";
+    // echo "<p><b>hello</b></p>";
+    // echo "<p>hello2</p>";
+
+    $loggedIn = false;
+    if ($loggedIn == true) {
+      echo "You are logged in";
+    } else {
+      echo "Please log in";
+    }
+
 
     // /**
     //  * Front to the WordPress application. This file doesn't do anything, but loads
@@ -33,5 +41,6 @@
     // /** Loads the WordPress Environment and Template */
     // require( dirname( __FILE__ ) . '/wp-blog-header.php' );
     ?>
+
   </body>
 </html>
