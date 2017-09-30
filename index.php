15b3986429ef23e872ed337ea0dc9c0069859d07
Rose Wan <rose.wan@me.com>
2017-09-29 21:45:03 -0400

    https://www.youtube.com/watch?v=ZdP0KM49IVk. Calculating sums with . echo to paste

--- index.php
+++ index.php
@@ -8,23 +8,27 @@
   <body>
     <?php
 
-    echo "PHP";
-    /**
-     * Front to the WordPress application. This file doesn't do anything, but loads
-     * wp-blog-header.php which does and tells WordPress to load the theme.
-     *
-     * @package WordPress
-     */
+    $myvar = "This is a variable";
+    $number = 5;
+    $number2 = 3;
+    echo $sum = $number + $number2;
 
-    /**
-     * Tells WordPress to load the WordPress theme and output it.
-     *
-     * @var bool
-     */
-    define('WP_USE_THEMES', true);
-
-    /** Loads the WordPress Environment and Template */
-    require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+    // /**
+    //  * Front to the WordPress application. This file doesn't do anything, but loads
+    //  * wp-blog-header.php which does and tells WordPress to load the theme.
+    //  *
+    //  * @package WordPress
+    //  */
+    //
+    // /**
+    //  * Tells WordPress to load the WordPress theme and output it.
+    //  *
+    //  * @var bool
+    //  */
+    // define('WP_USE_THEMES', true);
+    //
+    // /** Loads the WordPress Environment and Template */
+    // require( dirname( __FILE__ ) . '/wp-blog-header.php' );
     ?>
   </body>
 </html>
