630c9402fa2ff6c75deeed47c8117e5fd5647a17
Rose Wan <rose.wan@me.com>
2017-10-02 21:49:35 -0400

    no duplicates in automobile list

--- automobile-list.php
+++ automobile-list.php
@@ -1,37 +1,6 @@
 
 <h1>All the Cars</h1>
-<?php
 
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
-
-  // sql to create table if it doesn't exist
-  $sql = "CREATE TABLE IF NOT EXISTS `tbl_automobiles` (
-    automobile_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    car_model VARCHAR(30) NOT NULL,
-    weight DECIMAL NOT NULL,
-    manufacture_year DATE
-  )";
-
-  if ($conn->query($sql) === TRUE) {
-      // echo "Table tbl_automobiles created successfully";
-  } else {
-      echo "Error creating table: " . $conn->error;
-  }
-  //adding values to tbl_automobiles list
-  //
-  // $insert = "IF (EXISTS (SELECT * FROM tbl_automobiles))
-  //   INSERT INTO tbl_automobiles (car_model, weight, manufacture_year)
-  //   VALUES ('Toyota Camry', '3190', '2009-01-01'), ('Honda Civic', '3045', '2016-02-03')";
-  // if ($conn->query($insert) === TRUE) {
-  //     echo "Inserted successfully";
-  // } else {
-  //     echo "Error inserting: " . $conn->error;
-  // }
-
-  ?>
 
   <table>
     <tr>
@@ -42,6 +11,7 @@
 
     <?php
     $select = "SELECT * from tbl_automobiles";
+
     if ($result = $conn->query($select)) {
       // echo "Got results";
     } else {
--- index.php
+++ index.php
@@ -27,6 +27,42 @@
       // Create connection
       $conn = new mysqli($servername, $username, $password, $db);
 
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+
+      // sql to create table if it doesn't exist
+      $sql = "CREATE TABLE IF NOT EXISTS `tbl_automobiles` (
+        automobile_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        car_model VARCHAR(30) NOT NULL,
+        weight DECIMAL NOT NULL,
+        manufacture_year DATE
+      )";
+
+      if ($conn->query($sql) === TRUE) {
+          // echo "Table tbl_automobiles created successfully";
+      } else {
+          echo "Error creating table: " . $conn->error;
+      }
+      //adding values to tbl_automobiles list
+
+      $select = "SELECT * from tbl_automobiles";
+      $hasCars = $conn->query($select)->num_rows > 0;
+
+      if (!$hasCars) {
+        $insert = "
+          INSERT INTO tbl_automobiles (car_model, weight, manufacture_year)
+          VALUES ('Toyota Camry', '3190', '2009-01-01'),
+          ('Honda Civic', '3045', '2016-02-03'),
+          ('Toyota Corolla', '3192', '2012-01-01')";
+        if ($conn->query($insert) === TRUE) {
+            // echo "Inserted successfully";
+        } else {
+            echo "Error inserting: " . $conn->error;
+        }
+      }
+
       require 'automobile-list.php';
 
     ?>
